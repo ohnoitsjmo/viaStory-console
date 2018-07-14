6a47247f39adcde91e9682be1a6871df790408fe
Josiah Pang <pang.josiah@gmail.com>
2018-07-14 10:31:21 -0700

    add viasat bootstrap

--- dist/main.js
+++ dist/main.js
@@ -41,7 +41,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<div class=\"margin\" id=\"fullPage\">\n  <app-header></app-header>\n  <div class=\"content\">\n    <router-outlet></router-outlet>\n  <div>\n  <app-footer></app-footer>\n</div>  "
+module.exports = "<div class=\"margin\" id=\"fullPage\">\r\n  <app-header></app-header>\r\n  <div class=\"content\">\r\n    <router-outlet></router-outlet>\r\n  <div>\r\n  <app-footer></app-footer>\r\n</div>  "
 
 /***/ }),
 
@@ -199,7 +199,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<p class=\"text-success\">\n  footer works!\n</p>\n"
+module.exports = "<p class=\"text-success\">\r\n  footer works!\r\n</p>\r\n"
 
 /***/ }),
 
@@ -262,7 +262,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  gallery works!\n</p>\n"
+module.exports = "<p>\r\n  gallery works!\r\n</p>\r\n"
 
 /***/ }),
 
@@ -325,7 +325,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<p class=\"text-primary\">\n  header works!\n</p>\n"
+module.exports = "<p class=\"text-primary\">\r\n  header works!\r\n</p>\r\n"
 
 /***/ }),
 
@@ -388,7 +388,7 @@ module.exports = ""
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<ul>\n  <li *ngFor=\"let user of users | async\">\n      <pre>{{user.name}}</pre>\n  </li>\n</ul>\n\n<div *ngFor=\"let image of images\">\n\t<img src=\"{{image.url}}\"/>\n</div>"
+module.exports = "<ul>\r\n  <li *ngFor=\"let user of users | async\">\r\n      <pre>{{user.name}}</pre>\r\n  </li>\r\n</ul>\r\n\r\n<div *ngFor=\"let image of images\">\r\n\t<img src=\"{{image.url}}\"/>\r\n</div>"
 
 /***/ }),
 
@@ -511,7 +511,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/jmo/Documents/viaStory/src/main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\pangj\Documents\Viasat-Hackathon\viaStory-console\src\main.ts */"./src/main.ts");
 
 
 /***/ })
