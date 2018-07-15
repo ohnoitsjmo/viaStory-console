c183891aa8d80a05e288557c89053004d058941e
Justin Mo <jmo02@calpoly.edu>
2018-07-14 18:54:06 -0700

    social media

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Images } from '../images';
 import { Observable } from 'rxjs';
+import { UserService } from '../user.service';
 import 'rxjs/add/operator/map';
 
 import { AngularFirestore } from 'angularfire2/firestore';
 
-export interface Image { id: string; imagePath: string; imageURL: string; imageName: string; maintTs: number; }
+export interface Image { user: UserService, id: string; imagePath: string; imageURL: string; imageName: string; maintTs: number; }
 
 @Component({
   selector: 'app-home',
@@ -18,7 +19,7 @@ export class HomeComponent implements OnInit {
   users: Observable<any[]>;
   images: Images[];
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private user: UserService) {
       this.users = db.collection('/users').valueChanges();
       db.collection('/posts').doc('Image').valueChanges().subscribe(results =>
         {
