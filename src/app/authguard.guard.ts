a4f3e3c6f3fc8b98d63f4e63d9d086559a426376
Justin Mo <jmo02@calpoly.edu>
2018-07-14 23:19:36 -0700

    fuck this

--- src/app/authguard.guard.ts
+++ src/app/authguard.guard.ts
@@ -4,32 +4,29 @@ import { Router } from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import { AngularFirestore } from 'angularfire2/firestore';
 import { UserService } from './user.service';
+import 'rxjs/add/operator/toPromise';
+
+
 import 'rxjs/add/operator/map';
+import { Observable } from '../../node_modules/rxjs';
 
 @Injectable()
 export class AuthguardGuard implements CanActivate {
-  users:any[]
+  userr:any;
 	constructor(private user: UserService, private db: AngularFirestore, private router:Router, private http:HttpClient) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Promise<boolean> {
         return new Promise<boolean>(resolve => {
-          this.db.collection('/userAuth').doc('VQ3Y5ufxgGSAChehiMhY').valueChanges().subscribe(results =>
-            {
-              this.users = results['authentication'];
-              for (var i=0; i<this.users.length; i++) {
-                if (this.user.username == this.users[i].username) {
-                  if (this.user.password == this.users[i].password) {
-                    resolve(true);
-                    return;
-                  }
-                }
+            this.db.collection('userAuth', ref => ref.where("username", '==', this.user.username)).valueChanges().subscribe(ref => {
+              if (ref[0]["password"] == this.user.password) {
+                resolve(true);
+              } else {
+                this.router.navigate(['']);
+                resolve(false);
               }
-              this.router.navigate(['']);
-              resolve(false);
-            }
-          )
-      });
-   }
-}
+            });
+          })
+      };
+   };
