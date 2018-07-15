60d0902b3a9d94f37f10846a82b427bb0bbd48e1
Josiah Pang <pang.josiah@gmail.com>
2018-07-15 12:37:58 -0700

    add embed map and locations dropdown

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.get('*', function (req, res) {
 //   return res.json({username:});
 // });
 
-app.listen(8080);
-console.log("Server running on port 8080");
+app.listen(3000);
+console.log("Server running on port 3000");
--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-map',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit() {
   }
